c0642631a60b82226895d61923bfd2083763bd8d
korobochko-coder <korobochko.a@gmail.com>
2023-02-06 15:22:18 -0500

    LocationButton

---
+++ src/CurrentLocation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CurrentLocation() {
+  return (
+    <div className="CurrentLocation">
+      <button className="LocationButton">📍 My Location</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import SearchEngine from "./SearchEngine";
+import CurrentLocation from "./CurrentLocation";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SearchEngine />
+    <CurrentLocation />
   </React.StrictMode>
 );
 
